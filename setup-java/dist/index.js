c2250ed924f65739ad546c8b71e5c186303facf4
udkg <49506680+udkg@users.noreply.github.com>
2020-11-12 16:19:38 -0800

    Update index.js

--- setup-java/dist/index.js
+++ setup-java/dist/index.js
@@ -1252,7 +1252,7 @@ exports.debug = debug; // for test
 /***/ (function(module) {
 
 module.exports = require("https");
-module.exports = require('semver-compare');
+//module.exports = require('semver-compare');
 
 /***/ }),
 
